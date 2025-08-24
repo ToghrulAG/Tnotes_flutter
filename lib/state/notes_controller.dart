f0586d026740db6a7cfd4d589ca7c3a05411a6fa
Toghrul AG <tgrlagl@proton.me>
2025-08-24 11:35:26 +0400

    added controller part

---
+++ lib/state/notes_controller.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../data/repos/notes_repo.dart';
+import '../data/models/note.dart';
+
+final notesRepoProvider = Provider<NotesRepo>((ref) => HiveNotesRepo());
+
+final notesProvider =
+    StateNotifierProvider<NotesController, AsyncValue<List<Note>>>(
+      (ref) => NotesController(ref)..refresh(),
+    );
+
+class NotesController extends StateNotifier<AsyncValue<List<Note>>> {
+  NotesController(this.ref) : super(AsyncValue.loading());
+  final Ref ref;
+
+  Future<void> refresh() async {
+    try {
+      state = const AsyncValue.loading();
+      final repo = ref.read(notesRepoProvider);
+      final notes = await repo.getAll();
+      state = AsyncValue.data(notes);
+    } catch (error, stackTrace) {
+      state = AsyncError(error, stackTrace);
+    }
+  }
+}
+
+
