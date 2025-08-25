b07d0dd5475e2e7af367c1e127239146e80f7cf0
Toghrul AG <tgrlagl@proton.me>
2025-08-25 20:54:56 +0400

    added create method for ui

--- lib/state/notes_controller.dart
+++ lib/state/notes_controller.dart
@@ -9,20 +9,19 @@ final notesProvider =
       (ref) => NotesController(ref)..refresh(),
     );
 
+
 class NotesController extends StateNotifier<AsyncValue<List<Note>>> {
   NotesController(this.ref) : super(AsyncValue.loading());
   final Ref ref;
 
   Future<void> refresh() async {
-    try {
-      state = const AsyncValue.loading();
-      final repo = ref.read(notesRepoProvider);
-      final notes = await repo.getAll();
-      state = AsyncValue.data(notes);
-    } catch (error, stackTrace) {
-      state = AsyncError(error, stackTrace);
-    }
+    final repo = ref.read(notesRepoProvider);
+    final notes = await repo.getAll();
+    state = AsyncValue.data(notes);
+  }
+  Future <void> create(Note note) async {
+    final repo = ref.read(notesRepoProvider);
+    await repo.create(note);
+    await refresh();
   }
 }
-
-
